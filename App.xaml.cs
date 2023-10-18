13a6035c486f9acb6db7c7a029363027f96819f5
AdamKurek <kurek.adam1@gmail.com>
2023-10-18 16:41:03 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AustralianFall
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
