bc8c8556d4fc760bdf52c74bdb33a7017e25ba82
AdamKurek <kurek.adam1@gmail.com>
2023-10-19 03:35:56 +0200

    added goofy ass image

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using SkiaSharp;
+using System.Reflection;
+using System.Runtime.ExceptionServices;
 
 namespace AustralianFall
 {
@@ -10,7 +12,7 @@ namespace AustralianFall
             InitializeComponent();
 
 
-            PaintSurface.PaintSurface += OnPaint;
+            PaintSurface.PaintSurface += OnPaintAsync;
             PaintSurface.SizeChanged += SizeChanged;
         }
 
@@ -18,13 +20,38 @@ namespace AustralianFall
         {
             c.Resize((float)PaintSurface.Width, (float)PaintSurface.Height);    
         }
-
-        private void OnPaint(object sender, SkiaSharp.Views.Maui.SKPaintSurfaceEventArgs e)
+        private async void OnPaintAsync(object sender, SkiaSharp.Views.Maui.SKPaintSurfaceEventArgs e)
         {
             var canvas = e.Surface.Canvas;
             canvas.Clear();
             c.Draw(canvas);
 
+            //svg.Load("fallingGuy.svg");
+
+            // Draw the SVG document to the canvas
+            //new SKFileStream("FallinGuyLogo")
+            string resourceID = "fallin.png";
+            var jdStream = FileSystem.Current.OpenAppPackageFileAsync(resourceID).Result;
+            var data = SKData.Create(jdStream);
+            var bitmap = SKBitmap.Decode(data);
+
+            // Or create an SKImage from the stream
+           // var image = SKImage.FromEncodedData(data);
+            if(bitmap.DrawsNothing)
+            {
+                bitmap.Dispose();
+            }
+            try { 
+                canvas.DrawBitmap(bitmap,new SKPoint(1,1));
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            bitmap.Dispose();
+
+            //canvas.DrawImage(image, new SKPoint(0, 0));
+            //image.Dispose();
         }
     }
 }
