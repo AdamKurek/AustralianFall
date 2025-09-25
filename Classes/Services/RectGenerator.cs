355c4cd8174c2bddf1277530ed8ccbe9c3a73470
AdamKurek <61457610+AdamKurek@users.noreply.github.com>
2025-09-25 19:04:39 +0200

    Fix RectGenerator syntax errors

--- Classes/Services/RectGenerator.cs
+++ Classes/Services/RectGenerator.cs
@@ -1,15 +1,19 @@
-﻿using SkiaSharp;
+using System.Collections.Generic;
+using System.Linq;
+using SkiaSharp;
 
 namespace AustralianFall.Classes.Services
 {
     internal static class RectGenerator
     {
-        private readonly static Random ran = new();
+        private static readonly Random ran = new();
         private static readonly float Widtho = 1000;
 
-        internal static SKRect GenerateRect(List<SKRect> rects, float height = 100f, float width = 100f, float lowerPt = 700f){
+        internal static SKRect GenerateRect(List<SKRect> rects, float height = 100f, float width = 100f, float lowerPt = 700f)
+        {
             bool overlaps = true;
             SKRect newRect = new SKRect();
+
             while (overlaps)
             {
                 float left = (float)(ran.NextDouble() * (Widtho - width));
@@ -17,7 +21,8 @@ namespace AustralianFall.Classes.Services
                 newRect = new SKRect(left, top, left + width, top + height);
                 overlaps = rects.Any(rect => rect.IntersectsWith(newRect));
             }
+
             return newRect;
-            }
         }
+    }
 }
